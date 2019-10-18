b6b2a4f870b3f7836af3fc0b395f4d685f25905f
Ellpeck <megamaximal@gmail.com>
2019-10-18 23:34:13 +0200

    made commands a lot nicer

--- Program.cs
+++ Program.cs
@@ -8,9 +8,8 @@ using System.Text.RegularExpressions;
 namespace AdventOfCode {
     public static class Program {
 
-        private static readonly Dictionary<int, List<ISolution>> AllSolutions = new Dictionary<int, List<ISolution>>();
-
         public static void Main(string[] args) {
+            var allSolutions = new Dictionary<int, List<ISolution>>();
             foreach (var type in Assembly.GetExecutingAssembly().GetTypes()) {
                 if (!type.IsClass)
                     continue;
@@ -20,90 +19,73 @@ namespace AdventOfCode {
                 var inst = (ISolution) Activator.CreateInstance(type);
                 var year = inst.GetYear();
 
-                if (!AllSolutions.ContainsKey(year))
-                    AllSolutions.Add(year, new List<ISolution>());
-                AllSolutions[year].Add(inst);
+                if (!allSolutions.ContainsKey(year))
+                    allSolutions.Add(year, new List<ISolution>());
+                allSolutions[year].Add(inst);
             }
-            foreach (var solutions in AllSolutions.Values)
+            foreach (var solutions in allSolutions.Values)
                 solutions.Sort((s1, s2) => s1.GetDay().CompareTo(s2.GetDay()));
 
-            if (args.Length == 1) {
-                var arg = args[0];
-                if (SolveAllYear(arg))
+            var commands = new Dictionary<Regex, Action<Match>>();
+            // Solve a year
+            commands.Add(new Regex(@"^\d+$"), match => {
+                var year = int.Parse(match.Value);
+                if (!allSolutions.TryGetValue(year, out var solutions)) {
+                    Console.WriteLine($"The year {year} hasn't been solved yet");
                     return;
-                if (SolveSpecific(arg))
-                    return;
-                if (SolveLatest(arg))
-                    return;
-                if (SolveAll(arg))
-                    return;
-                if (List(arg))
-                    return;
-            }
-            Console.WriteLine("Usage: dotnet run <[year] | [year]/[day] | latest | all | list>");
-        }
-
-        private static bool SolveAllYear(string arg) {
-            if (!int.TryParse(arg, out var year))
-                return false;
-            if (!AllSolutions.TryGetValue(year, out var solutions)) {
-                Console.WriteLine($"The year {year} hasn't been solved yet");
-                return true;
-            }
-            foreach (var solution in solutions)
-                Solve(solution);
-            return true;
-        }
-
-        private static bool SolveSpecific(string arg) {
-            var match = Regex.Match(arg, @"(\d*)/(\d*)");
-            if (!int.TryParse(match.Groups[1].Value, out var year))
-                return false;
-            if (!int.TryParse(match.Groups[2].Value, out var day))
-                return false;
-            if (!AllSolutions.TryGetValue(year, out var solutions)) {
-                Console.WriteLine($"The year {year} hasn't been solved yet");
-                return true;
-            }
-            var solution = solutions.Find(s => s.GetDay() == day);
-            if (solution == null) {
-                Console.WriteLine($"The day {day} of {year} hasn't been solved yet");
-                return true;
-            }
-            Solve(solution);
-            return true;
-        }
-
-        private static bool SolveLatest(string arg) {
-            if (!arg.Equals("latest"))
-                return false;
-            var highestYear = AllSolutions.Keys.Max();
-            var solutions = AllSolutions[highestYear];
-            var solution = solutions.OrderBy(s => s.GetDay()).Last();
-            Solve(solution);
-            return true;
-        }
-
-        private static bool SolveAll(string arg) {
-            if (!arg.Equals("all"))
-                return false;
-            foreach (var entry in AllSolutions.OrderBy(entry => entry.Key)) {
-                foreach (var solution in entry.Value) {
+                }
+                foreach (var solution in solutions)
                     Solve(solution);
+            });
+            // Solve a specific day
+            commands.Add(new Regex(@"^(\d+)/(\d+)$"), match => {
+                var year = int.Parse(match.Groups[1].Value);
+                var day = int.Parse(match.Groups[2].Value);
+                if (!allSolutions.TryGetValue(year, out var solutions)) {
+                    Console.WriteLine($"The year {year} hasn't been solved yet");
+                    return;
+                }
+                var solution = solutions.Find(s => s.GetDay() == day);
+                if (solution == null) {
+                    Console.WriteLine($"The day {day} of {year} hasn't been solved yet");
+                    return;
+                }
+                Solve(solution);
+            });
+            // Solve latest
+            commands.Add(new Regex("^latest$"), match => {
+                var highestYear = allSolutions.Keys.Max();
+                var solutions = allSolutions[highestYear];
+                var solution = solutions.OrderBy(s => s.GetDay()).Last();
+                Solve(solution);
+            });
+            // Solve all
+            commands.Add(new Regex("^all$"), match => {
+                foreach (var entry in allSolutions.OrderBy(entry => entry.Key)) {
+                    foreach (var solution in entry.Value) {
+                        Solve(solution);
+                    }
+                }
+            });
+            // List all
+            commands.Add(new Regex("^list$"), match => {
+                foreach (var (key, value) in allSolutions.OrderBy(entry => entry.Key)) {
+                    Console.WriteLine($"Year {key}:");
+                    foreach (var solution in value)
+                        Console.WriteLine($"  Day {solution.GetDay()}: {solution.GetName()}");
+                }
+            });
+
+            if (args.Length == 1) {
+                foreach (var (regex, command) in commands) {
+                    var match = regex.Match(args[0]);
+                    if (match.Success) {
+                        command(match);
+                        return;
+                    }
                 }
             }
-            return true;
-        }
-
-        private static bool List(string arg) {
-            if (!arg.Equals("list"))
-                return false;
-            foreach (var (key, value) in AllSolutions.OrderBy(entry => entry.Key)) {
-                Console.WriteLine($"Year {key}:");
-                foreach (var solution in value)
-                    Console.WriteLine($"  Day {solution.GetDay()}: {solution.GetName()}");
-            }
-            return true;
+            Console.WriteLine("Usage: dotnet run <[year] | [year]/[day] | latest | all | list>");
         }
 
         private static void Solve(ISolution solution) {
